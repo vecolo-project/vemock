c0c2bf3defcc77595f6ca228d3a7619fb49a3b73
Nouu <noelarrieulacoste@yahoo.fr>
2021-05-11 20:49:02 +0200

    add better behavior

--- state/State.py
+++ state/State.py
@@ -2,6 +2,22 @@ import datetime
 import random
 
 
+def is_day():
+    return 8 <= datetime.datetime.now().hour <= 19
+
+
+def is_morning():
+    return 8 <= datetime.datetime.now().hour <= 12
+
+
+def is_middle_day():
+    return 11 <= datetime.datetime.now().hour <= 15
+
+
+def is_afternoon():
+    return 12 <= datetime.datetime.now().hour <= 19
+
+
 class State:
     def __init__(self, battery, auto_off, used_seats, max_seats, charging_power=0.0, max_charging_power=20.0,
                  active=True):
@@ -17,20 +33,18 @@ class State:
         self.next_state_discharge()
         self.next_state_active()
         self.next_state_charge()
+        self.next_state_seats()
 
     def next_state_discharge(self):
-        current_time = datetime.datetime.now()
         # Elle se décharge régulièrement, un peu moins la nuit (vu que moins d'utilisation)
-        if 8 < current_time.hour < 18:
-            # During day
+        if is_day():
             # Se décharge plus vite si il y a beaucoup de vélos sur la borne
             discharge = 0.001 * self.used_seats
-
-            # Le random va tourner plus en faveur du chargement de la batterie si < 15%
-            if self.battery > 15:
-                discharge += round(random.uniform(0.000, 0.010), 3)
         else:
             discharge = 0.0002 * self.used_seats
+        # Le random va tourner plus en faveur du chargement de la batterie si < 15%
+        if self.battery > 15:
+            discharge += round(random.uniform(0.000, 0.010), 3)
 
         # La batterie ne doit pas dépasser 100% et descendre en dessous de 0
         self.battery = min(100., max(0., float("{:.4f}".format(self.battery - discharge))))
@@ -43,12 +57,29 @@ class State:
                 self.active = self.battery >= 15
 
     def next_state_charge(self):
-        current_time = datetime.datetime.now()
         # Elle se décharge régulièrement, un peu moins la nuit (vu que moins d'utilisation)
-        if 8 < current_time.hour < 20:
-            self.charging_power = round(max(0.,
-                                            min(self.max_charging_power,
-                                                self.charging_power + random.uniform(-self.max_charging_power / 50,
-                                                                                     self.max_charging_power / 50))), 4)
-        self.battery = round(min(100.,
-                                 max(0., self.battery + (self.charging_power / (self.max_charging_power * 100)))), 4)
+        if is_day():
+            random_charge = \
+                random.uniform(-self.max_charging_power / 50, self.max_charging_power / 20) \
+                    if is_middle_day() \
+                    else random.uniform(-self.max_charging_power / 50, self.max_charging_power / 50) \
+                    if is_morning() \
+                    else random.uniform(-self.max_charging_power / 50, self.max_charging_power / 40)
+
+            self.charging_power = min(self.max_charging_power,
+                                      max(0., float("{:.4f}".format(self.charging_power + random_charge))))
+        else:
+            self.charging_power = max(0., float("{:.4f}".format(self.charging_power - 0.1)))
+
+        self.battery = min(100.,
+                           max(0., float("{:.4f}".format(
+                               self.battery + (self.charging_power / 20000)))))
+
+    def next_state_seats(self):
+        randmax = 10 if is_day() else 50
+        if self.used_seats > 0:
+            if random.randint(0, randmax) == randmax:
+                self.used_seats -= 1
+        if self.used_seats < self.max_seats:
+            if random.randint(0, randmax) == randmax:
+                self.used_seats += 1
--- state/state_bootstrap.py
+++ state/state_bootstrap.py
@@ -8,4 +8,5 @@ def loop_state(frequency_wait, history_path, state):
         state.next_state()
         print(f'State : {state.__dict__}')
         state_file.save_state(history_path, state)
+        state_file.append_state(state)
         time.sleep(frequency_wait)
