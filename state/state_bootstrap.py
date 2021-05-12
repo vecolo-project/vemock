d18d99b1733b2931a50ac48f2bd6c2e84750590c
Nouu <noelarrieulacoste@yahoo.fr>
2021-05-12 23:48:28 +0200

    print state show date and time

--- state/state_bootstrap.py
+++ state/state_bootstrap.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from state import state_file
@@ -6,7 +7,7 @@ from state import state_file
 def loop_state(frequency_wait, history_path, state):
     while 1:
         state.next_state()
-        print(f'State : {state.__dict__}')
+        print(f'{datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")} : {state.__dict__}')
         state_file.save_state(history_path, state)
         state_file.append_state(state)
         time.sleep(frequency_wait)
