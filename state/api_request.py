968c0a3039e94ff508f401e31d758a8be40a13ae
Nouu <noelarrieulacoste@yahoo.fr>
2021-05-22 11:27:18 +0200

    add debug mode

--- state/api_request.py
+++ state/api_request.py
@@ -1,9 +1,7 @@
-import json
-
 import requests
 
 
-def submit_state(api_endpoint, state, jwt_token):
+def submit_state(api_endpoint, state, jwt_token, debug=True):
     headers = {'Authorization': 'Bearer ' + jwt_token}
     data = {
         'battery': state.battery,
@@ -12,6 +10,7 @@ def submit_state(api_endpoint, state, jwt_token):
         'used_seats': state.used_seats,
     }
     response = requests.post(api_endpoint, headers=headers, json=data)
-    print('url : ' + response.request.url)
-    print('headers : ', response.request.headers)
-    print('body : ', response.request.body)
+    if debug:
+        print('url : ' + response.request.url)
+        print('headers : ', response.request.headers)
+        print('body : ', response.request.body)
--- state/state_bootstrap.py
+++ state/state_bootstrap.py
@@ -4,11 +4,12 @@ from state.state_file import save_state, print_state, append_state
 from state.api_request import submit_state
 
 
-def loop_state(frequency_wait, history_path, state, jwt_token, api_endpoint):
-    while 1:
+def loop_state(frequency_wait, history_path, state, jwt_token, api_endpoint, debug=True):
+    while True:
         state.next_state()
-        print_state(state)
-        append_state(state)
-        submit_state(api_endpoint, state, jwt_token)
+        submit_state(api_endpoint, state, jwt_token, debug)
         save_state(history_path, state)
+        if debug:
+            print_state(state)
+            append_state(state)
         sleep(frequency_wait)
