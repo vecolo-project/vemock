3c86689b2c46bb9a885db396bb0d8075e7e647a8
Nouu <noelarrieulacoste@yahoo.fr>
2021-05-23 12:29:46 +0200

    replace request lib (heavy in memory consumption) by simple http client

--- state/api_request.py
+++ state/api_request.py
@@ -1,16 +1,23 @@
-import requests
+# import requests
+# import httpx
+import http.client
+import json
 
 
 def submit_state(api_endpoint, state, jwt_token, debug=True):
-    headers = {'Authorization': 'Bearer ' + jwt_token}
+    headers = {'Authorization': 'Bearer ' + jwt_token, 'Content-type': 'application/json'}
     data = {
         'battery': state.battery,
         'charging_power': state.charging_power,
         'active': state.active,
         'used_seats': state.used_seats,
     }
-    response = requests.post(api_endpoint, headers=headers, json=data)
+    conn = http.client.HTTPSConnection(api_endpoint)
+    conn.request('POST', '/', json.dumps(data), headers)
     if debug:
-        print('url : ' + response.request.url)
-        print('headers : ', response.request.headers)
-        print('body : ', response.request.body)
+        print(conn.getresponse().read().decode())
+    # print(response.read().decode())
+    # response = requests.post(api_endpoint, headers=headers, json=data)
+    #     print('url : ' + response.request.url)
+    #     print('headers : ', response.request.headers)
+    #     print('body : ', response.request.body)
